spatial averaging, error fix in logging, better input checks

src/descriptors/SOAP.py
@@ -54,11 +54,12 @@ class SOAP_descriptor():
             values=torch.tensor(selected_atoms, dtype=torch.int64).unsqueeze(-1),
         )
 
-    def calculate(self, systems):
-        
+    def calculate(self, systems, sel_samples=None):
+        if sel_samples is None:
+            sel_samples = self.sel_samples
         soap = self.calculator(
             systems,
-            selected_samples=self.sel_samples,
+            selected_samples=sel_samples,
             selected_keys=self.sel_keys,
         )
         

src/methods/ivac_methods.py
@@ -294,11 +294,11 @@ class IVAC(FullMethodBase):
 
         for i, trafo in enumerate(self.transformations):
             torch.save(
+                torch.tensor(trafo.eigvals.copy()),
                 self.label + f"_center{self.descriptor.centers[i]}" + f"_eigvals.pt",
-                torch.tensor(trafo.eigvals),
             )
 
             torch.save(
+                torch.tensor(trafo.eigvecs.copy()),
                 self.label + f"_center{self.descriptor.centers[i]}" + f"_eigvecs.pt",
-                torch.tensor(trafo.eigvecs),
             )